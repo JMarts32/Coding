1253e0a7ba418e5c83b77305b00d960a7bcc2879
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2022-09-20 13:58:28 -0500

    Partialy accepted solution

---
+++ LeetCode/src/Easy/RomanToInteger/Solution.java
@@ -0,0 +1,39 @@
+package Easy.RomanToInteger;
+
+import java.util.Map;
+import static java.util.Map.entry;
+
+public class Solution {
+
+    private final Map<String,Integer> roman = Map.ofEntries(
+            entry("I",1),
+            entry("V",5),
+            entry("X",10),
+            entry("L",50),
+            entry("C",100),
+            entry("D",500),
+            entry("M",1000)
+    );
+
+    public int romanToInt(String s) {
+        int contador = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int uno = roman.get(String.valueOf(s.charAt(i)));
+            int dos = roman.get(String.valueOf(s.charAt(i+1)))
+            if (uno < dos && i<s.length()-1){
+                contador -= roman.get(String.valueOf(s.charAt(i)));
+                contador += roman.get(String.valueOf(s.charAt(i+1)));
+                i ++;
+            }else {
+                contador += roman.get(String.valueOf(s.charAt(i)));
+            }
+        }
+        return contador;
+    }
+
+    public static void main(String[] args) {
+        Solution solucion = new Solution();
+        solucion.romanToInt("MCMXCIV");
+    }
+
+}
