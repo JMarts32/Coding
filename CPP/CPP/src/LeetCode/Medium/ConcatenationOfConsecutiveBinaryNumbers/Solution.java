bfb91e9dc7b01f41146b86d425c1c6e8f1ca5230
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2022-10-12 10:05:37 -0500

    Recreation of the projecto due to problems with the code

---
+++ CPP/CPP/src/LeetCode/Medium/ConcatenationOfConsecutiveBinaryNumbers/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.Medium.ConcatenationOfConsecutiveBinaryNumbers;
+
+public class Solution {
+    public int concatenatedBinary(int n) {
+
+        int contador = 0;
+        int posicion = 0;
+
+        for (int i = n; i <= n ; i--){
+
+        }
+        return 0;
+    }
+}
