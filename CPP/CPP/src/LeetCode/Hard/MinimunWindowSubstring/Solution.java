b237e6440d63b9cbadfde917694c19309ccd292e
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2022-10-22 11:01:04 -0500

    Comments of the problem

--- CPP/CPP/src/LeetCode/Hard/MinimunWindowSubstring/Solution.java
+++ CPP/CPP/src/LeetCode/Hard/MinimunWindowSubstring/Solution.java
@@ -1,5 +1,28 @@
 package LeetCode.Hard.MinimunWindowSubstring;
 
+/*
+Given two strings s and t of lengths m and n respectively,
+return the minimum window substring of s such that every character in t
+(including duplicates) is included in the window. If there is no such
+substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+A substring is a contiguous sequence of characters within the string.
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+ */
 public class Solution {
     public static void main(String[] args) {
 
