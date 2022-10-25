98d28cf5223d0550ccd439a0a8ab78770ea3013a
JMarts32 <j.santosa@uniandes.edu.co>
2022-10-25 12:33:00 -0500

    Comments updated 2.0

--- CPP/CPP/src/LeetCode/Easy/CheckIfTwoStringsArraysAreEquivalent/Solution.java
+++ CPP/CPP/src/LeetCode/Easy/CheckIfTwoStringsArraysAreEquivalent/Solution.java
@@ -1,7 +1,10 @@
 package LeetCode.Easy.CheckIfTwoStringsArraysAreEquivalent;
 
 /*
+Given two string arrays word1 and word2, return true if the two arrays represent
+the same string, and false otherwise.
 
+A string is represented by an array if the array elements concatenated in order forms the string
  */
 public class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
