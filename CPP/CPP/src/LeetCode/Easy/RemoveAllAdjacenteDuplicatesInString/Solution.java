d8a3f9493f56cee9118dd027e4950f43679ccdd6
JMarts32 <j.santosa@uniandes.edu.co>
2022-11-12 11:53:38 -0500

    Solution not working

--- CPP/CPP/src/LeetCode/Easy/RemoveAllAdjacenteDuplicatesInString/Solution.java
+++ CPP/CPP/src/LeetCode/Easy/RemoveAllAdjacenteDuplicatesInString/Solution.java
@@ -4,26 +4,31 @@ import java.util.*;
 
 public class Solution {
     public String removeDuplicates(String s) {
-        String[] sl = s.split("");
         StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder(2);
+        StringBuilder sb2 = new StringBuilder(s);
         Map<String, Integer> sm = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if (sl.length == 0) {
-                sb1.append(sl[i]);
-            } else {
-                String comp = Character.toString(sb1.charAt(0));
-                if (comp == sl[i]) {
-                    sb1.append(sl[1]);
-                } else{
-                    if (sb1.length() > 1){
-                        String sb1S = sb1.toString();
-                        sb2.delete(sb2.indexOf(Character.toString(sb2.charAt(i-1))),i-1);
-                        i = -1;
-                    }else {
-                        sb1.delete(i,i);
-                        sb1.append(sl[i]);
+            if (!(i == s.length()-1)){
+                String[] sl = sb2.toString().split("");
+                if (sb1.isEmpty()) {
+                    sb1.append(sl[i]);
+                } else {
+                    String comp = Character.toString(sb1.charAt(0));
+                    if (comp.equals(sl[i])) {
+                        sb1.append(sl[1]);
+                    } else{
+                        if (sb1.length() > 1){
+                            String sb1S = sb1.toString();
+                            sb2.delete(sb2.indexOf(Character.toString(sb2.charAt(i-1))),i);
+                            i=i==0?i-1:i-2;
+                            sb1.delete(0,sb1.length());
+                        }else {
+                            if (i!=-1){
+                                sb1.delete(0,1);
+                                sb1.append(sl[i]);
+                            }
+                        }
                     }
                 }
             }
@@ -32,5 +37,8 @@ public class Solution {
         return sb2.toString();
     }
 
-
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.removeDuplicates("abbac"));
+    }
 }
