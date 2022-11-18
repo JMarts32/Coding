9093f5d988c0ec2dd22f2e15bfdccb9f9e0a7bc8
JMarts32 <j.santosa@uniandes.edu.co>
2022-11-18 10:46:58 -0500

    Working solution

--- CPP/CPP/src/LeetCode/TopInterviewQuestions/TwoSum/Solution.java
+++ CPP/CPP/src/LeetCode/TopInterviewQuestions/TwoSum/Solution.java
@@ -27,80 +27,20 @@ Output: [0,1]
  */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        // We are putting all the numbers into a tree map to organize them
-        TreeMap<Integer, String> tm = new TreeMap<>();
-        Map<Integer, String> hm = new HashMap<>();
+        Map<Integer, Integer> hm = new HashMap<>();
 
         // Result array
         int[] res = new int[2];
 
-        // Add the elements to the hashmap
-        for (int i = 0; i < nums.length; i++) {
-            if (hm.containsKey(nums[i])) {
-                hm.replace(nums[i], hm.get(nums[i]) + i);
-            } else {
-                hm.put(nums[i], String.valueOf(i));
+        int numsl = nums.length;
+
+        for (int i = 0; i<numsl;i++ ){
+            if (hm.containsKey(target-nums[i])){
+                res[1] = i;
+                res[0] = hm.get(target-nums[i]);
             }
+            hm.put(nums[i],i);
         }
-
-        // number of total numbers in the array -> -1 is the final position in the array
-        int rec = hm.keySet().size();
-        int cont = 0;
-
-        while (cont < rec) {
-
-            // reviews the last object in the array of keys
-            int last = (Integer) hm.keySet().toArray()[rec-1];
-
-            //Reviews the first object in the array of keys
-            int first = (Integer) hm.keySet().toArray()[0];
-
-            if (last < first){
-                last = (Integer) hm.keySet().toArray()[0];
-                first = (Integer) hm.keySet().toArray()[rec-1];
-            }
-
-            if (hm.get(first).length() > 1 && first*2 == target){
-                res[0] = Integer.parseInt(hm.get(first).split("")[0]);
-                res[1] = Integer.parseInt(hm.get(first).split("")[1]);
-                return res;
-            }
-
-            if (last + first <= target) {
-                if (last == target && hm.containsKey(0)) {
-                    res[0] = Integer.parseInt(hm.get(0).split("")[0]);
-                    res[1] = Integer.parseInt(hm.get(last).split("")[0]);
-                    return res;
-                }
-
-
-                if (hm.get(last).length() > 1 && last*2 == target){
-                    res[0] = Integer.parseInt(hm.get(last).split("")[0]);
-                    res[1] = Integer.parseInt(hm.get(last).split("")[1]);
-                    return res;
-                }
-
-                if (last + first > target){
-                    hm.remove(last);
-                    hm.remove(first);
-                } else {
-
-                    if (last + first < target) hm.remove(first);
-
-                    if (last + first == target){
-                        res[0] = Integer.parseInt(hm.get(first));
-                        res[1] = Integer.parseInt(hm.get(last));
-                        return res;
-                    }
-                }
-
-            }else {
-                hm.remove(last);
-            }
-            rec = hm.keySet().size();
-        }
-        System.out.println(res);
         return res;
     }
 
