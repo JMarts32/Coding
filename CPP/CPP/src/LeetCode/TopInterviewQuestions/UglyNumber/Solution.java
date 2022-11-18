2f3a4515bad98205d44fe6217c40a06b50fa2a34
JMarts32 <j.santosa@uniandes.edu.co>
2022-11-18 11:04:01 -0500

    Solution Accepted

--- CPP/CPP/src/LeetCode/TopInterviewQuestions/UglyNumber/Solution.java
+++ CPP/CPP/src/LeetCode/TopInterviewQuestions/UglyNumber/Solution.java
@@ -1,4 +1,24 @@
 package LeetCode.TopInterviewQuestions.UglyNumber;
 
 public class Solution {
+    //Positive integer
+    //Prime numbers 2,3, and 5
+
+    public boolean isUgly(int n){
+        // Ugly number is a positive integer
+        if (n < 1) return false;
+        // No prime factors, therefore all its prime factors are limited
+        if (n == 1)return true;
+
+        // Look for the prime factors, if it passes 5, then return false
+        for (int i = 2; i < n; i++){
+            if (i > 5) return false;
+
+            while (n%i == 0){
+                n /= i;
+            }
+        }
+
+        return true;
+    }
 }
